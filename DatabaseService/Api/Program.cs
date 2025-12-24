dae742bf69e61653ded69871aebca0c868e6b863
bkorshunov <mrdrakula164@gmail.com>
2025-12-24 20:52:31 +0500

    Api

--- DatabaseService/Api/Program.cs
+++ DatabaseService/Api/Program.cs
@@ -1,3 +1,6 @@
+using Dal;
+using Logic;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDal(builder.Configuration.GetConnectionString("Db"));
+builder.Services.AddLogic();
 
 var app = builder.Build();
 
