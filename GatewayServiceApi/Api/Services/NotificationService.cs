a9a8f66b4804a8dc2872bdbbc22b811a12b00b48
bkorshunov <mrdrakula164@gmail.com>
2025-12-25 19:45:41 +0500

    Update NotificationService.cs

--- GatewayServiceApi/Api/Services/NotificationService.cs
+++ GatewayServiceApi/Api/Services/NotificationService.cs
@@ -2,18 +2,22 @@
 using DatabaseConnectionLib.Models.StoreMessage;
 using GatewayServiceApi.Interfaces;
 using GatewayServiceApi.Models;
-using MessageQueueConnectionLib.Interfaces;
+using MessageQueueConnectionLib;
+using MessageQueueConnectionLib.ConnectionServices.Interfaces;
 
 namespace GatewayServiceApi.Services;
 
 public class NotificationService : INotificationService
 {
     private readonly IDatabaseConnectionService _databaseService;
+    private readonly IConnectionService _messageQueueService;
 
     public NotificationService(
-        IDatabaseConnectionService databaseService)
+        IDatabaseConnectionService databaseService,
+        IConnectionService messageQueueService)
     {
         _databaseService = databaseService;
+        _messageQueueService = messageQueueService;
     }
 
     public async Task<Guid> PublishMessageAsync(NotificationDto dto)
@@ -27,6 +31,15 @@ public class NotificationService : INotificationService
             Subject = dto.Subject,
             Metadata = dto.Metadata,
             Recipient = dto.Recipient
+        });
+        _messageQueueService.SendNotificationAsync(new MessageRequest
+        {
+            Id = id,
+            ChannelType = dto.ChannelType,
+            Content = dto.Content,
+            Recipient = dto.Recipient,
+            Subject = dto.Subject,
+            CreatedAt = DateTime.UtcNow
         });
         return id;
     }
