f2f849a95ef4fba34ffffbcbfc58d1941f85ca21
LeonKote <roman892265@gmail.com>
2026-01-02 20:42:53 +0500

    Updated solution and integrated MonitoringConnectionLib

--- GatewayServiceApi/Program.cs
+++ GatewayServiceApi/Program.cs
@@ -1,6 +1,7 @@
 using DatabaseConnectionLib;
 using GatewayServiceApi;
 using MessageQueueConnectionLib;
+using MonitoringConnectionLib;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,12 +12,17 @@ builder.Services.AddDatabaseConnectionLib();
 builder.Services.AddMessageQueueConnectionLib();
 builder.Services.AddServices();
 
+builder.AddMonitoringMetrics();
+builder.AddLogging();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseMonitoringEndpoints();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
